9cfa3619ce684175c94126eb1b16f0f46c857eb3
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-04-15 16:39:08 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-plain-blog</title>
+</head>
+
+<body>
+    <header>
+        <p>ciao!</p>
+    </header>
+
+</body>
+
+</html>
